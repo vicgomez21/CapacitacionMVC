479eed01158b504b7d584cfd23e882f4c387bd80
vicgomez21 <victorgomezcarrascosa@gmail.com>
2026-09-02 23:53:19 -0600

    subiendo vista del form principal

---
+++ practica1/TallerMVC_1/CapaVista_Taller/frmPrincipal.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CapaVista_Taller
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvConsulta = new System.Windows.Forms.DataGridView();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsulta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvConsulta
+            // 
+            this.dgvConsulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsulta.Location = new System.Drawing.Point(40, 216);
+            this.dgvConsulta.Name = "dgvConsulta";
+            this.dgvConsulta.RowHeadersWidth = 51;
+            this.dgvConsulta.RowTemplate.Height = 24;
+            this.dgvConsulta.Size = new System.Drawing.Size(720, 216);
+            this.dgvConsulta.TabIndex = 0;
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Location = new System.Drawing.Point(192, 88);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(200, 72);
+            this.btnConsulta.TabIndex = 1;
+            this.btnConsulta.Text = "Consultar";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.dgvConsulta);
+            this.Name = "frmPrincipal";
+            this.Text = "frmPrincipal";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsulta)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvConsulta;
+        private System.Windows.Forms.Button btnConsulta;
+    }
+}
