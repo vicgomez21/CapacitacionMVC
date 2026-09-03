eba117205e28155081538a073e91faa97512219d
vicgomez21 <victorgomezcarrascosa@gmail.com>
2026-09-03 00:11:48 -0600

    subiendo Clases conexion y sentencia

---
+++ practica1/TallerMVC_1/CapaModelo_Taller/Conexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo_Taller
+{
+    class Conexion
+    {
+        public OdbcConnection conexion()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn=sistema_reparto;");
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Conexion exitosa");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error de conexion: " + ex.Message);
+            }
+            return conn;
+        }
+
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("Error Se Desconecto");
+            }
+        }
+    }
+}
---
+++ practica1/TallerMVC_1/CapaModelo_Taller/Sentencia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo_Taller
+{
+    public class Sentencia
+    {
+        Conexion conn = new Conexion();
+        public OdbcDataAdapter llenarTbl(string nombreTabla)
+        {
+            string sSQL = "SELECT * FROM " + nombreTabla+";";
+            //llenarse con la informacion de la tabla que se le pase por parametro
+            OdbcDataAdapter daSentencias = new OdbcDataAdapter(sSQL, conn.conexion());
+            return daSentencias;
+        }
+    }
+}
