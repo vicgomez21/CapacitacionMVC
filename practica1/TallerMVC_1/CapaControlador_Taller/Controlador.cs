07c09a99b8337d862f0e236a329d1c81b601b7d9
vicgomez21 <victorgomezcarrascosa@gmail.com>
2026-09-03 00:19:27 -0600

    Subiendo Clase Controlador

---
+++ practica1/TallerMVC_1/CapaControlador_Taller/Controlador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo_Taller;
+
+namespace CapaControlador_Taller
+{
+    public class Controlador
+    {
+        Sentencia sentencia = new Sentencia();
+        public DataTable llenarTbl(string nombreTabla)
+        {
+        
+            OdbcDataAdapter daControlador =sentencia.llenarTbl(nombreTabla);
+           
+            DataTable dtControlador = new DataTable();
+
+            daControlador.Fill(dtControlador);
+            
+            return dtControlador;
+        }
+    }
+}
